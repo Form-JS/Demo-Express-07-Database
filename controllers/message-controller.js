8b290dd42132ab370314096c4eb425e9dc962a0b
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-16 13:27:49 +0100

    Cleanup

--- controllers/message-controller.js
+++ controllers/message-controller.js
@@ -13,13 +13,14 @@ const messageController = {
         // Affichage le detail d'un message
         const { id } = req.params;
 
+        // TODO Recup les datas
+
         res.render('message/detail', { title: `Detail du message ${id}` });
     },
 
     // GET
     messageFormGET: (req, res) => {
         // Permet d'afficher la page avec le formulaire
-
         res.render('message/newMessage', { title: 'Nouveau message', errors: null, data: {} });
     },
 
@@ -29,6 +30,7 @@ const messageController = {
         messageSchema.validate(req.body, { abortEarly: false })
             .then((data) => {
                 console.log('data', data);
+
                 // TODO Ajouter dans la DB 
 
                 res.redirect('/message');
